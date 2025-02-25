bf32c1d0a54706acc8548b6965b7bd8b7b5dec8c
Ahmed Shaban <ahmed530sh@gmail.com>
2025-02-25 15:01:07 +0200

    add service get all posts

--- src/Modules/posts/services/post.service.js
+++ src/Modules/posts/services/post.service.js
@@ -2,32 +2,32 @@
 import { postModel } from "../../../DB/models/post.model.js"
 
 
-// جبت كل البوست ب الكومنتات الي عليها بصحاب كل كومينت
-// export const getAllPosts=async(req,res)=>{
-//     const post=await postModel.find().populate({
-
-//         path:'commentData',
-//         select:'content',
-//         populate:[{
-//             path:'ownerId',
-//             select:'username'
-//         }]
-
-//     }
-//     )
-//     res.status(200).json({post})
-// }
-
-
+//جبت كل البوست ب الكومنتات الي عليها بصحاب كل كومينت
 export const getAllPosts=async(req,res)=>{
-    const {limit,page}=req.query
-    const post=await postModel.paginate({},
-        {
-            limit,
-            page,
-            
+    const post=await postModel.find().populate({
 
-        })
-    
+        path:'commentData',
+        select:'content',
+        populate:[{
+            path:'ownerId',
+            select:'username'
+        }]
+
+    }
+    )
     res.status(200).json({post})
 }
+
+
+// export const getAllPosts=async(req,res)=>{
+//     const {limit,page}=req.query
+//     const post=await postModel.paginate({},
+//         {
+//             limit,
+//             page,
+            
+
+//         })
+    
+//     res.status(200).json({post})
+// }
